e7aea2ec055f10897d912d2ef478ef9e2d759eea
owlprojection <bagus.yanuar613@gmail.com>
2025-12-28 15:44:57 +0700

    feat: create util SuccessResponse

--- pkg/util/response.go
+++ pkg/util/response.go
@@ -1,9 +1,19 @@
 package util
 
+import "github.com/gofiber/fiber/v2"
+
 type APIResponse[T any] struct {
 	Code    int             `json:"code"`
 	Message string          `json:"message"`
 	Data    T               `json:"data"`
 	Meta    *PaginationMeta `json:"meta,omitempty"`
 	Errors  any             `json:"errors,omitempty"`
+}
+
+func SuccessResponse[T any](ctx *fiber.Ctx, message string, data T) error {
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"code":    fiber.StatusOK,
+		"message": message,
+		"data":    data,
+	})
 }
